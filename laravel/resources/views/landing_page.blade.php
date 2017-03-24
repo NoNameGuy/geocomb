6ee86c9b67ac171d4c03c1b24c37f623675c9768
NoNameGuy <paulojose.m.vieira@gmail.com>
2017-03-24 18:20:34 +0000

    CSS
    Algin CSS without colors

--- laravel/resources/views/landing_page.blade.php
+++ laravel/resources/views/landing_page.blade.php
@@ -1,81 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-				<meta name="viewport" content="width=device-width, initial-scale=1">
-					<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-					<title>!GeoComb!</title>
-					<!-- Bootstrap -->
-          <link href="css/bootstrap.min.css" rel="stylesheet">
-					<link rel="stylesheet" type="text/css" href="{{asset('css/style.css') }}">
-						<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-						<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-						<!--[if lt IE 9]>
-						<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-						<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-						<![endif]-->
-					</head>
-					<body>
-						<h1>Preço dos Combustiveis</h1>
+<head>
+	<title>!GeoComb!</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-						 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style.css') }}">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 
-						<a href="{{action('LandingController@fetchData')}}">Fetch data Aveiro</a>
-						<a href="{{action('LandingController@fetchStationData')}}">Fetch data 165954 station</a>
-						<a href="{{action('LandingController@mapsApi')}}">Maps Api</a>
+	</head>
+					<body><nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-						<div class="divPesquisaPostos">
-							<h2>Pesquisar Postos:</h2>
-							<div class="divEsqHome">
-                <p> Por Localização </p>
-								<select>
-									<?php echo "Distritos: ";
-                      foreach ($districts as $key => $district) {
-                          echo "
+<div class="container-fluid text-center">
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <!--<p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>-->
+    </div>
+    <div class="col-sm-8 text-left">
+      <h1 class="center">Preço dos Combustiveis</h1>
+      <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>-->
+			<a href="{{action('LandingController@fetchData')}}">Fetch data Aveiro</a>
+			<a href="{{action('LandingController@fetchStationData')}}">Fetch data 165954 station</a>
+			<a href="{{action('LandingController@mapsApi')}}">Maps Api</a>
 
-          									<option> $district->name </option>";
+			<hr>
 
-                      }
-                  ?>
-								</select>
-							</div>
-							<div class="divDrtHome">
-                <p> Por Combustivel </p>
-                <select>
-                  <?php echo "Distritos: ";
-                      foreach ($districts as $key => $district) {
-                          echo "
+			<h2 class="center">Pesquisar Postos:</h2>
+		<div class="row">
+			<div class="col-sm-6">
+        <p class="center"> Por Localização :</p>
+				<br>
+				<select>
+					<?php echo "Distritos: ";
+            foreach ($districts as $key => $district) {
+            	echo "<option> $district->name </option>";
+            }
+          ?>
+				</select>
+			</div>
+			<div class="col-sm-6">
+				<p class="center"> Por Combustível :</p>
+				<br>
+				<select>
+					<?php echo "Distritos: ";
+            foreach ($districts as $key => $district) {
+            	echo "<option> $district->name </option>";
+            }
+          ?>
+				</select>
+				</div>
+			</div>
+    </div>
 
-                            <option> $district->name </option>";
+    <div class="col-sm-2 sidenav">
+    </div>
+  </div>
+</div>
 
-                      }
-                  ?>
-                </select>
-              </div>
-						</div>
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
 
-          <div class="divPostosBaratosProx">
-							<div class="divEsqHome">
-                <h4> Os 5 Mais Baratos: </h4>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-              </div>
-							<div class="divDrtHome">
-                <h4> Os 5 Mais Próximos: </h4>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-                <p> Os 5 Mais Baratos: </p>
-              </div>
-						</div>
-						<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-						<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-						<!-- Include all compiled plugins (below), or include individual files as needed -->
-						<script src="js/bootstrap.min.js"></script>
-					</body>
-				</html>
+</body>
+</html>
