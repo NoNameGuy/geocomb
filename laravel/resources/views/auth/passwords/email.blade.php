f4ae08839ba77f4c0ed3a0b6c5d0e68ecd03e32c
NoNameGuy <paulojose.m.vieira@gmail.com>
2017-09-08 15:19:48 +0100

    Verification mail
    Bug solved

--- laravel/resources/views/auth/passwords/email.blade.php
+++ laravel/resources/views/auth/passwords/email.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
+@include('header')
+        <div class="col-lg-12 text-left" style="background-color:grey">
+          <h1 style="text-align:center">Reset Password</h1>
+              <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
